c5adf008b988148472c486f6c2a97fff055a0f65
Branislav Havrila <branislav.havrila@ricardo.ch>
2017-12-14 14:05:38 +0100

    Add Sample project

---
+++ Ricardo.MBProgressHUD.iOS.Sample/ViewController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using UIKit;
+
+namespace Ricardo.RMBProgressHUD.iOS.Sample
+{
+    public partial class ViewController : UIViewController
+    {
+        protected ViewController(IntPtr handle) : base(handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            MBProgressHUD hud = MBProgressHUD.ShowHUD(View, true);
+
+            hud.Label.Text = "Loading...";
+
+            hud.Show(true);
+            hud.Hide(true, 3.0f);
+        }
+    }
+}
